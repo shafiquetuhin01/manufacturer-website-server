f2417861e0487d524f3177d3c19839630cfc2f27
BiasDesk1 <shafiquetuhin01>
2022-05-23 11:58:06 +0600

    services inserted in mongodb

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
       await client.connect();
-      const serviceCollection = client.db('doctors_portal').collection('tools');
+      const serviceCollection = client.db('bd_tools').collection('tools');
         
       app.get('/service', async (req, res) => {
         const query = {};
