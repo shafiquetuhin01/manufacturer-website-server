b87f3f646d1f40b4034936b73296e5d9a5d1364f
BiasDesk1 <shafiquetuhin01>
2022-05-24 16:17:58 +0600

    purchase page updating

--- index.js
+++ index.js
@@ -23,6 +23,13 @@ async function run() {
         const services = await cursor.toArray();
         res.send(services);
       });
+
+      app.get('/service/:id', async(req, res) =>{
+        const id = req.params.id;
+        const query = {_id: ObjectId(id)};
+        const booking = await serviceCollection.findOne(query);
+        res.send(booking);
+      })
     }
     finally {
   
